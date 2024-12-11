77a1ec83579efc5d337b03a4ccec63e4601908b7
Elizabeth Garrido <105178087+elizabethG9@users.noreply.github.com>
2024-12-11 00:19:46 -0300

    refactor: remove MongoDB attributes from Id property

--- Src/Models/Career.cs
+++ Src/Models/Career.cs
@@ -2,8 +2,6 @@ using MongoDB.Bson.Serialization.Attributes;
 
 public class Career 
 {
-    [BsonId]
-    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-    public int Id { get; set; }
+    public int Id { get; set; } 
     public string Name { get; set; } = null!;
 }
--- Src/Models/Subject.cs
+++ Src/Models/Subject.cs
@@ -3,9 +3,8 @@ using MongoDB.Bson.Serialization.Attributes;
 
 public class Subject
 {
-    [BsonId]
-    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-    public int Id { get; set; } 
+
+    public int Id { get; set; }
     public string Code { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string Department { get; set; } = null!;
