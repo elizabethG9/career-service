f5c296a0cd546fdee4064c198d809945ec1c557f
Elizabeth Garrido <105178087+elizabethG9@users.noreply.github.com>
2024-12-11 00:16:59 -0300

    feat: add database and grpc configurations

--- Program.cs
+++ Program.cs
@@ -1,15 +1,30 @@
-using CareerManagementService.Data.DataSeeder;
+
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDB")
+);
+builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+{
+    var settings = serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
+builder.Services.AddScoped<DataContext>();
+builder.Services.AddScoped<DbSeeder>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddGrpc();  // Esta línea es importante
 
-builder.Services.AddSingleton<DataContext>();
-builder.Services.AddSingleton<DbSeeder>();
+
+
 
 builder.Services.AddScoped<ICareersRepository, CareerRepository>();
 builder.Services.AddScoped<ICareersService, CareersService>();
@@ -18,6 +33,12 @@ builder.Services.AddScoped<ISubjectsService, SubjectsService>();
 
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
+    await seeder.SeedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -29,6 +50,11 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+app.MapGrpcService<SubjectsGrpcService>();
+app.MapGrpcService<CareersGrpcService>();
+app.MapGet("/", () => "Hello World!");
+
 
 
 app.Run();
